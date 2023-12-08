7f6f9a3495b42924f65daa3f00f3478be2d1045f
Timofey Goretskiy <69255946+Kenshinnoai@users.noreply.github.com>
2023-12-08 23:32:52 +0300

    Add files via upload
    made some first headers for my list

---
+++ mylib.c
@@ -0,0 +1,8 @@
+#include "mylib.h"
+#include <stdio.h>
+
+void sum(int a, int b)
+{
+    int summa = a + b;
+    printf("%d\n", summa);
+}
---
+++ mylib.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+void sum(int a, int b)
+{
+    int summa = a + b;
+    printf("%d", summa);
+}
