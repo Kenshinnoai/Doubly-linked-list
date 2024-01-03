df95dd702ccbe5c1c21a7a4b573687d3d4c280f3
Timofey Goretskiy <69255946+Kenshinnoai@users.noreply.github.com>
2024-01-03 17:16:07 +0300

    added function to work with list from console (there're some mistakes)

--- main.c
+++ main.c
@@ -1,33 +1,17 @@
 #include "list.h"
 #include <stdio.h>
 
-static void test_long_list(void);
+/*static void test_long_list(void);*/
 
 int main()
 {
-    printf("First output: \n");
-    Node *node_1 = NULL;
-    node_1 = list_create_node(8);
-    list_out(node_1);
-
-    printf("Second output: \n");
-    Node *node_2 = NULL;
-    node_2 = list_create_node(45);
-    list_insert(node_1, node_2);
-    list_out(node_1);
-
-    printf("Third output: \n");
-    list_delete_node(node_2);
-    list_out(node_1);
-
-    list_delete(node_1);
-
-    test_long_list();
+    choice();
+    /*test_long_list();*/
 
     return 0;
 }
 
-static void test_long_list(void)
+/*static void test_long_list(void)
 {
     int i;
     Node* head = list_create_node(0);
@@ -51,4 +35,4 @@ void test_3(void)
 {
     List* list = list_create();
     list_prepend(list, ...);
-}
+}*/
