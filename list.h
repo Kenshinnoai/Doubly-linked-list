443cfcabd3919b7be9c05921f1a845d9c2bd287d
Timofey Goretskiy <69255946+Kenshinnoai@users.noreply.github.com>
2024-01-08 15:09:01 +0300

    deleted mistakes with return's in functions

--- list.h
+++ list.h
@@ -6,13 +6,13 @@ typedef struct Node Node;
 typedef struct List List;
 
 /* Создание элемента списка. */
-extern Node* list_create_node();
+extern Node* list_create_node(void);
 
 /*инициализирует узел списка (присваевает указателям на первый и последний элементы значения NULL)*/
 extern void list_init(List* list);
 
 /* Проверяет, пустой ли список. */
-extern int list_is_empty(List* list);
+extern void list_is_empty(List* list);
 
 /* Вставка в начало списка.
  * Возвращает 0 в случае успеха, либо не ноль в случае ошибки.
@@ -20,7 +20,7 @@ extern int list_is_empty(List* list);
 extern int list_prepend(List* list);
 
 /* Вставка в конец списка. */
-extern Node* list_append(List* list);
+extern int list_append(List* list);
 
 /* Вставка после insnode. */
 extern void list_insert(Node* insnode, Node* newnode);
@@ -29,14 +29,14 @@ extern void list_insert(Node* insnode, Node* newnode);
 extern void list_delete(List* list);
 
 /* Удаление элемента списка. */
-extern void list_delete_node(Node *node);
+extern void list_delete_node(Node* node);
 
 /* Вывод списка поэлементно на экран. */
 extern void list_out(List* list);
 
 
 /* Меню для манипуляции функциями через консоль */
-extern int choice(void);
+extern void choice(void);
 
 /*Ищет номер удаляемого узла в списке, а потом удаляет его*/
 extern void delete_node_by_number(List* list);
