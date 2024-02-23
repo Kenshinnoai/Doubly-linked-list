42fecbf379f072f355f602692935db4e8b804dbe
Timofey Goretskiy <69255946+Kenshinnoai@users.noreply.github.com>
2024-02-24 01:17:24 +0300

    added new func. getNumber, made list.h easier to read, moved comments on the right side of the code

--- list.h
+++ list.h
@@ -5,41 +5,36 @@ typedef struct Node Node;
 
 typedef struct List List;
 
-extern List* list_create(void);
+    /* Далее - вспомогательные функции */
 
-/*инициализирует узел списка (присваевает указателям на первый и последний элементы значения NULL)*/
-extern void list_init(List* list);
+//*******************************************************************
 
-/* Проверяет, пустой ли список. */
-extern int list_is_empty(List* list);
+extern List* list_create(void);                     /* создание списка*/
 
-/* Вставка в начало списка.
- * Возвращает 0 в случае успеха, либо не ноль в случае ошибки.
- */
-extern int list_prepend(List* list, int value);
+extern void list_init(List* list);                  /* инициализация списка*/
 
-/* Вставка в конец списка. */
-extern int list_append(List* list, int value);
+extern Node* list_create_node(void);                /* создание нового узла*/
 
-/* Создание элемента списка. */
-extern Node* list_create_node(void);
+extern int list_is_empty(List* list);               /* проверка на пустоту списка*/
 
-/* Вставка после insnode. */
-extern void list_insert(Node* insnode, Node* newnode);
+extern int getNumber();                             /* получение от пользователя числа*/
 
-/* Удаление всего списка. */
-extern void list_delete(List* list);
 
-/* Удаление элемента списка. */
-extern void list_delete_node(Node* node);
+    /* Далее - фукции для работы с пользователем */
 
-/* Вывод списка поэлементно на экран. */
-extern void list_out(List* list);
+//*******************************************************************
 
-/*Ищет номер удаляемого узла в списке, а потом удаляет его*/
-extern void delete_node_by_number(List* list);
 
-/*Тот же list_insert, но тут мы передаём в функцию стрктуру list*/
-extern void list_insert_using_list(List* list);
+extern int list_prepend(List* list, int value);     /* добавление нового узла в начало списка*/
 
-#endif // MYLIB_H
+extern int list_append(List* list, int value);      /* добавление нового узла в конец списка*/
+
+extern void list_delete_node(List *list);           /* удаление узла из списка*/
+
+extern void list_delete(List* list);                /* удаление передаваемого списка*/
+
+extern void list_out(List* list);                   /* вывод всех значений элементов списка*/
+
+extern void swap(List* list);                       /* меняет значения у двух узлов*/
+
+#endif
